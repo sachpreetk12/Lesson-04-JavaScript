41e5803a653a8bc043cd48eaf4e58d7eb3fb3362
Sachpreet Kaur <Sachpreet Kaur@LAPTOP-VSUV5AG8>
2020-10-08 19:02:41 -0400

    im done!!

--- src/components/TicTacToe/Board/index.jsx
+++ src/components/TicTacToe/Board/index.jsx
@@ -4,13 +4,59 @@ import Styles from './styles';
 import Won from './Won';
 
 const Board = () => {
-  
+  const [player, setPlayer] = useState(1);
+  const [piece, setPiece] = useState('x');
+  const [won, setWon] = useState(false);
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const changePlayer = location => {
+    const clone = [...board];
+    clone[location] = piece;
+    setBoard(clone);
+    setPlayer(player === 1 ? 2 : 1);
+    setPiece(piece === 'x' ? 'o' : 'x');
+  };
+
+  const resetBoard = () => {
+    setBoard(Array(9).fill(null));
+    setWon(false);
+    setPiece('x');
+    setPlayer(1);
+  }
+
+  useEffect(() => {
+    const possibleWins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
+    ];
+
+    for (let [pos1, pos2, pos3] of possibleWins) {
+      if (!board[pos1] || !board[pos2] || !board[pos3]) continue;
+
+      if (board[pos1] === board[pos2] && board[pos2] === board[pos3]) setWon(true);
+    }
+  }, [player]);
 
   return (
     <>
-      
+      {won ? (
+        <Won/>
+      ) : null}
+
       <Styles.Board>
-        
+        {board.map((boardPiece, i) => (
+          <Square
+            key={i}
+            piece={boardPiece}
+            changePlayer={() => changePlayer(i)}
+          />
+        ))}
       </Styles.Board>
       <button onClick={resetBoard}>Reset</button>
     </>
--- src/components/TicTacToe/index.jsx
+++ src/components/TicTacToe/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../shared/Header';
+import Board from './Board';
 
 const TicTacToe = () => {
   return (
     <>
       <Header title="Let's do this thang"/>
+      <Board/>
     </>
   );
 }
